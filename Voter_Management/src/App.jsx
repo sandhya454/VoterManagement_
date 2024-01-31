7d9d6e2056bd5b9204e8582e31797406fea1736c
sandhya454 <104482875+sandhya454@users.noreply.github.com>
2024-01-31 10:13:39 +0530

    modified responsive for users

--- Voter_Management/src/App.jsx
+++ Voter_Management/src/App.jsx
@@ -6,11 +6,8 @@ import Dashboard from './assets/Components/MainPage/Dashboard/Dashboard'
 import Users from './assets/Components/MainPage/Users/Users'
 import Booths from './assets/Components/MainPage/Booths/Booths'
 import Reports from './assets/Components/MainPage/Reports.jsx/Reports'
-<<<<<<< HEAD
 import BoothList from './assets/Components/MainPage/Booths/BoothList'
-=======
 import Page from './assets/Components/MainPage/Page/page'
->>>>>>> 910bac8f12363d7ce8e28749342062ced92f8d72
 
 
 
@@ -25,8 +22,9 @@ function App() {
         <Route path='/' element={<Login/>}/>
         <Route path='dashboard' element={<Dashboard/>}>
           <Route path='users' element={<Users/>}/>
-          <Route path='booths' element={<Booths/>}/>
-           <Route path='boothlist' element={<BoothList/>}/>         
+         
+          <Route path='allocated' element={<Booths/>}/> 
+          <Route path='allocated/boothlist' element={<BoothList/>}/>         
           <Route path='reports' element={<Reports/>}/>
           <Route path='page' element={<Page/>}/>
         </Route>
